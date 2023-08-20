8d3ea5e2b1d0e884315659ae64895ccf56f128bc
daotriet05 <daohoangminhtriet@gmail.com>
2023-08-21 00:08:44 +0700

    Fail in connecting website to gg sheet. In debugging

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ------- About tab -------- 
 let openedTab="Skills";
 function openTab(tabName){
     tabs=document.getElementsByClassName(openedTab);
@@ -11,6 +12,7 @@ function openTab(tabName){
     openedTab=tabName;
 }
 
+// ---------- Side Menu --------- 
 const sideMenu=document.getElementById('sideMenu');
 function openMenu(){
     sideMenu.style.right='0';
@@ -18,3 +20,14 @@ function openMenu(){
 function closeMenu(){
     sideMenu.style.right='-200px';
 }
+
+// --------- Contact Form --------
+/*const scriptURL = 'https://script.google.com/macros/s/AKfycbz1o8V0jdHH5qOvots2hNILFxXuJhwWn3kJi_u8zFF9uzdQ_-QPrIzj1E2mTHKUZo0n/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})*/
