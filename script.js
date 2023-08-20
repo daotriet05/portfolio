9563f3785339a3bdce7c6c3f6ff39ae2aeda7060
daotriet05 <daohoangminhtriet@gmail.com>
2023-08-20 23:02:37 +0700

    finished Contact section and made web responsive

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ function openTab(tabName){
     }
     openedTab=tabName;
 }
+
+const sideMenu=document.getElementById('sideMenu');
+function openMenu(){
+    sideMenu.style.right='0';
+}
+function closeMenu(){
+    sideMenu.style.right='-200px';
+}
